ad186d0c3a0a69734ce820ac82d0e0ae5d4da5bf
welch3587 <welch3587@users.noreply.github.com>
2018-12-13 08:26:51 -0500

    Updating script for Lloyd's engagement 20181213
    The vm image doesn't have the engine installed yet so adding the tar step to install it. Also adding the manager to the local hosts file since the engine VM appears to need that.

--- ReconfigureEngine.sh
+++ ReconfigureEngine.sh
@@ -1,6 +1,11 @@
 directorName=$1
 directorPort=$2
 INSTALLDIR=$3
+cd /opt
+tar xvzf DSEngineLinux64.tar.gz
+
+printf "\n$1 lloydstestmanager.eastus.cloudapp.azure.com" > director.txt
+cat director.txt >> /etc/hosts
 
 cd $INSTALLDIR
 ./engine.sh stop
