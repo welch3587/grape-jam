5128ef08c950ac4f6dd8b0086d127b4e33101960
welch3587 <welch3587@users.noreply.github.com>
2019-08-07 07:42:38 -0400

    Updated for SSL and InstallDir

--- ReconfigureEngine.sh
+++ ReconfigureEngine.sh
@@ -1,14 +1,22 @@
 directorName=$1
-directorPort=$2
-INSTALLDIR=$3
-cd /opt
-tar xvzf DSEngineLinux64.tar.gz
+directorIP=$2
+directorPort=$3
+INSTALLDIR=$4
+sslenabled=$5
 
-printf "\n10.0.0.7 qahpcca120-2.eastus.cloudapp.azure.com" > director.txt
+printf "\n${directorIP} ${directorName}" > director.txt
 cat director.txt >> /etc/hosts
 
 cd $INSTALLDIR
+
 ./engine.sh stop
-./configure.sh -s ${directorName}:${directorPort}
+
+if [${sslenabled,,} = "true"]
+then
+    ./configure.sh -s ${directorName}:${directorPort} -l y
+else
+   ./configure.sh -s ${directorName}:${directorPort}
+fi
+
 ./engine.sh start
 
