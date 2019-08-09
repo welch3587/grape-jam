34b79f3a3e6b7f77b5dfc6fa8bfb9a4327bdee2b
welch3587 <welch3587@users.noreply.github.com>
2019-08-09 12:18:19 -0400

    Updated to support SSL

--- ReconfigureEngine.ps1
+++ ReconfigureEngine.ps1
@@ -1,23 +1,37 @@
 param(
+[string]$directorName,
+[string]$directorIP,
+[string]$directorPort,
 [string]$dsFolder,
-[string]$director,
-[string]$directorPort
+[string]$sslEnabled
 )
 
 Stop-Service DataSynapse
 
 #$dsFolder="c:\tibco\datasynapse\engine"
-#$director="10.1.0.6"
+#$directorIP="10.1.0.6"
 #$directorPort="8000"
+
+Set-HostsEntry -IPAddress $directorIP -HostName $directorName -Description "GridServer Director"
 
 Remove-Item -Recurse -Force "$($dsFolder)\\profiles"
 
 $datfile="$($dsFolder)\\intranet.dat"
-$contents = [IO.File]::ReadAllText($datfile)
-$httPrefixDelim="://"
-$endpos=$contents.IndexOf($httPrefixDelim)+$httPrefixDelim.Length
-$newPrefix=$contents.Substring(0,$endpos)
 
-"$($newPrefix)$($director):$($directorPort)/livecluster/public_html/register/register.jsp".Trim() | Out-File -Encoding Ascii $datfile
+#We originally did the following to capture whether the engine was set up
+#for HTTP or HTTPS. Now we're setting it explicitly based on the value
+#of sslEnabled.
+#$contents = [IO.File]::ReadAllText($datfile)
+#$httPrefixDelim="://"
+#$endpos=$contents.IndexOf($httPrefixDelim)+$httPrefixDelim.Length
+#$newPrefix=$contents.Substring(0,$endpos)
+
+if($sslEnabled.ToLower() -eq "true") {
+   $newPrefix="https://"
+} else {
+   $newPrefix="http://"
+}
+
+"$($newPrefix)$($directorIP):$($directorPort)/livecluster/public_html/register/register.jsp".Trim() | Out-File -Encoding Ascii $datfile
 
 Start-Service DataSynapse
